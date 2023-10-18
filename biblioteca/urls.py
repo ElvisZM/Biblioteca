685174cad44cf4029c51b884f5ccc61cb49a03ef
Elvis Zhune <elfabri28@gmail.com>
2023-10-18 14:42:26 +0200

    terminar en casa

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('libros/listar',views.listar_libros,name='lista_libros'),
+    path('libros/<int:id_libro>/', views.dame_libro, name='dame_libro'),
 ]
